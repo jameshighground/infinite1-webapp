e3daaad76dea199d5ff380790b600b1128eb9262
ldh5545 <eogml5545@naver.com>
2022-10-22 16:41:07 +0900

    day

--- src/interface.ts
+++ src/interface.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+export type MyPosition = {
+  real_lng: number;
+  real_lat: number;
+};
 export type SimplePrayType = {
   lat: number;
   lng: number;
@@ -13,7 +17,7 @@ export type PrayType = {
   real_lat: number;
   real_lng: number;
   content: string;
-  refId: number;
+  refid: number;
   craetedate: string;
   updatedate: string;
 };
